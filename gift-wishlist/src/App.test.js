56c168ecf0478c0094824703a0e8007e241c6309
EddyWayz <96652868+EddyWayz@users.noreply.github.com>
2025-08-25 18:03:54 +0200

    test: update header text

--- gift-wishlist/src/App.test.js
+++ gift-wishlist/src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByRole('heading', {
+    name: /Geschenke für Eddy und Joanne/i,
+  });
+  expect(headerElement).toBeInTheDocument();
 });
