be72a4361e790ffa1f8c78fb39f70b67aae7c028
EddyWayz <96652868+EddyWayz@users.noreply.github.com>
2025-08-25 18:18:59 +0200

    Add recipient filtering for gifts

--- gift-wishlist/server/server.js
+++ gift-wishlist/server/server.js
@@ -59,7 +59,7 @@ app.post('/api/gifts/:id/reserve', (req, res) => {
 
 // Endpoint to add a new gift
 app.post('/api/gifts', (req, res) => {
-  const { name, description, link, imageUrl, price } = req.body;
+  const { name, description, link, imageUrl, price, recipient } = req.body;
 
   fs.readFile(giftsFilePath, 'utf8', (err, data) => {
     if (err) {
@@ -75,6 +75,7 @@ app.post('/api/gifts', (req, res) => {
       link,
       imageUrl,
       price,
+      recipient,
       reservedBy: null
     };
 
